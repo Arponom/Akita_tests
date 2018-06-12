e5420682149f6cc022f635305901a479c41d1046
Arponom <31740888+Arponom@users.noreply.github.com>
2018-06-13 01:20:25 +0300

    first

---
+++ src/main/java/pages/SearchPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import ru.alfabank.alfatest.cucumber.annotations.Name;
+import ru.alfabank.alfatest.cucumber.annotations.Optional;
+import ru.alfabank.alfatest.cucumber.api.AkitaPage;
+
+@Name("Страница поиска гугл")
+public class SearchPage extends AkitaPage {
+
+    @FindBy(xpath = ".//*[(@title='Поиск')]")
+    @Name("Поиск")
+    public SelenideElement searchField;
+
+    @Optional
+    @FindBy(xpath = ".//*[(@value='Поиск в Google')]" )
+    @Name("Найти")
+    public SelenideElement searchButton;
+
+    @Optional
+    @FindBy(xpath = "//*[@id=\"tads\"]/ol/li[1]/div[2]/cite" )
+    @Name("Описание товара")
+    public SelenideElement fieldSearch;
+
+    @Optional
+    @FindBy(xpath = "//*[@id=\"vn1s0p1c0\"]" )
+    @Name("Описание товара2")
+    public SelenideElement fieldSearch2;
+
+
+
+
+}
+
