643c0dd5169771a91ddd046f4a9bf3e2e09a8b5a
Arponom <31740888+Arponom@users.noreply.github.com>
2018-06-26 20:14:08 +0300

    Внес изменения в функцию поиска мин температуры

--- src/main/java/steps/findMinTemperatureInArray.java
+++ src/main/java/steps/findMinTemperatureInArray.java
@@ -7,14 +7,15 @@ import cucumber.api.java.ru.Тогда;
 //import entities.Repositories;
 //import helpers.GitHubSearchPageHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.INTERNAL;
 import org.openqa.selenium.By;
 import ru.alfabank.alfatest.cucumber.api.AkitaScenario;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import static com.codeborne.selenide.Selenide.$;
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
 import static ru.alfabank.tests.core.helpers.PropertyLoader.tryLoadProperty;
@@ -27,15 +28,23 @@ public class findMinTemperatureInArray {
     @Тогда("^вывести минимальное значение из списка \"([^\"]*)\"$")
     public void selectRandomElementFromList(String listName) {
         List<SelenideElement> listOfElementsFromPage = akitaScenario.getCurrentPage().getElementsList(listName);
-        listOfElementsFromPage.get(getRandom(listOfElementsFromPage.size())).shouldBe(Condition.visible).click();
+        //listOfElementsFromPage.get(getRandom(listOfElementsFromPage.size())).shouldBe(Condition.visible).click();
+        List<String> elementsListText = listOfElementsFromPage.stream()
+                .map(element -> element.getText().trim().toLowerCase().substring(1))
+                .collect(toList());
 
-        Integer elementForCompare = Integer.MAX_VALUE;
+        List<Integer> listInt = new ArrayList<Integer>(elementsListText.size());
+        for (String current:elementsListText) {
+            listInt.add(Integer.parseInt(current));
+        }
+        Integer minValue = Collections.min(listInt);
+        /*Integer elementForCompare = Integer.MAX_VALUE;
         for (SelenideElement selenideElement: listOfElementsFromPage){
             if (Integer.parseInt(selenideElement.getSearchCriteria().substring(1))<elementForCompare){
                 elementForCompare=Integer.parseInt(selenideElement.getSearchCriteria().substring(1));
             }
-        }
-        akitaScenario.write("Выведено минимальное значение из списка: " + elementForCompare);
+        }*/
+        akitaScenario.write("Выведено минимальное значение из списка: " + minValue);
 
     }
 
